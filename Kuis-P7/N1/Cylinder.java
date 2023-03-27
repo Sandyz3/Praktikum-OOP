e9e88febe946503150b3ab44ba6a7f14659c465d
Sandyz3 <109643950+Sandyz3@users.noreply.github.com>
2023-03-27 12:15:47 +0700

    Update Cylinder.java

--- Kuis-P7/N1/Cylinder.java
+++ Kuis-P7/N1/Cylinder.java
@@ -1,41 +1,40 @@
 public class Cylinder extends Circle{
-    double height;
+    private double height;
 
-    public Cylinder () {
-
+    public Cylinder() {
+        super(1.0, "red");
+        this.height = 1.0;
     }
+
     public Cylinder(double radius) {
-        super(radius);
+        super(radius, "red");
+        this.height = 1.0;
     }
 
     public Cylinder(double radius, double height) {
-        super(radius);
+        super(radius, "red");
         this.height = height;
     }
 
-    public Cylinder(double radius, String color, double height) {
+    public Cylinder(double radius, double height, String color) {
         super(radius, color);
         this.height = height;
     }
 
     public double getHeight() {
-        return height;
+        return this.height;
     }
 
     public void setHeight(double height) {
         this.height = height;
     }
+
     public double getVolume() {
-        return super.getArea() * this.height;
+        return 3.14 * (super.getRadius()*super.getRadius()) * this.height;
     }
 
-
-    @Override
     public String toString() {
-        return "Cylinder{" +
-                "height=" + height +
-                ", radius=" + radius +
-                ", color='" + color + '\'' +
-                '}';
+        return "Cylinder[height="+this.height+",radius="+super.getRadius()+",color="+super.getColor()+"]";
     }
 }
+
