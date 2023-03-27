4313b2974c11f8a297bfe5879412c3e34b8bf921
Sandyz3 <109643950+Sandyz3@users.noreply.github.com>
2023-03-27 12:14:15 +0700

    Update Circle.java

--- Kuis-P7/N1/Circle.java
+++ Kuis-P7/N1/Circle.java
@@ -1,38 +1,44 @@
 public class Circle {
-    double radius;
-    String color;
+    private double radius;
+    private String color;
 
-    public Circle() {
-
+    public Circle(){
+        this.radius = 1.0;
+        this.color = "red";
     }
-    public Circle(double radius) {
-        this.radius = radius;}
+
+    public Circle(double radius){
+        this.radius = radius;
+        this.color = "red";
+    }
+
     public Circle(double radius, String color) {
         this.radius = radius;
         this.color = color;
     }
-    public double getRadius() {return this.radius;}
 
-    public void setRadius(double radius) {
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public void setRadius(double radius){
         this.radius = radius;
     }
 
     public String getColor() {
-        return color;
+        return this.color;
     }
 
-    public void setColor(String color) {
+    public void setColor(String color){
         this.color = color;
     }
-    public double getArea() {
-        return this.radius * this.radius * 3.14;
-    }
 
+    public double getArea() {
+        return 3.14 * this.radius * this.radius;
+    }
+    
     @Override
     public String toString() {
-        return "Circle{" +
-                "radius=" + radius +
-                ", color='" + color + '\'' +
-                '}';
+        return "Circle[radius="+this.radius+",color="+this.color+"]";
     }
 }
