e0ce2c7be42ab204236c4c547807c1c129781766
Sandyz3 <109643950+Sandyz3@users.noreply.github.com>
2023-04-10 09:54:41 +0700

    Update Main3.java

--- UTS_OOP/src/Main3.java
+++ UTS_OOP/src/Main3.java
@@ -4,5 +4,10 @@ public class Main3 {
         Penduduk pedagang = new Penduduk("pedagang",20000);
         Penduduk petani = new Penduduk("petani", 10000);
         Penduduk Pandaibesi = new Penduduk("Pandai besi", 13000);
+        
+        System.out.println(ksatria);
+        System.out.println(pedagang);
+        System.out.println(petani);
+        System.out.println(Pandaibesi);
     }
 }
